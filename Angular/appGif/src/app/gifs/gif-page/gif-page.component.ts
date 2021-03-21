3706080174ac6e97d0fae434f6d6e731765495d5
ColinTonyDev <colincitoheredia@gmail.com>
2021-03-20 20:53:52 -0600

    Delete Angular directory

--- Angular/appGif/src/app/gifs/gif-page/gif-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component} from '@angular/core';
-
-@Component({
-  selector: 'app-gif-page',
-  templateUrl: './gif-page.component.html'
-})
-export class GifPageComponent {
-
-}
--- Angular/bases-01/src/app/dbz/interfaces/personajes.interface.ts
+++
@@ -1,7 +0,0 @@
-
-// exportamos la interface de personaje.
-export interface Personaje {
-    nombre:string;
-    poder:number;
-}
-  
